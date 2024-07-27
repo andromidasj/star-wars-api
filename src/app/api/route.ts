d8aad258544715a57949f7b5b654e4a993aa2e2a
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2024-07-26 22:58:00 -0500

    handle error

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,10 @@
-import { DynamicIdParams } from "@/types";
 import { SWAPI } from "@/utils/SWAPI";
 
-export async function GET(_request: Request, { params }: DynamicIdParams) {
-  const list = await SWAPI.getOptions();
-  return Response.json(list);
+export async function GET(_request: Request) {
+  try {
+    const list = await SWAPI.getOptions();
+    return Response.json(list);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
