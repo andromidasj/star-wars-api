444092ac3cef94826a9913ed5f8576f7cd2c6856
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2024-07-28 00:08:23 -0500

    add filter for manually deleted entities

--- src/utils/entities/getAllEntities.ts
+++ src/utils/entities/getAllEntities.ts
@@ -1,8 +1,9 @@
 import { db } from "@/db";
-import { editedEntities } from "@/db/schema";
+import { deletedEntities, editedEntities } from "@/db/schema";
 import { EntityType, SwapiSchemas } from "@/types";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
+import { entityUrlToId } from "../entityIdUtils";
 import { mergeDataSets } from "../mergeDataSets";
 
 export async function getAllEntities({
@@ -19,10 +20,31 @@ export async function getAllEntities({
       .from(editedEntities)
       .where(eq(editedEntities.entityType, entityType));
 
-    const promiseArray = [swapiResponsePromise, localResultsPromise] as const;
+    const deletedEntitiesPromise = db
+      .select()
+      .from(deletedEntities)
+      .where(eq(deletedEntities.entityType, entityType));
 
-    const [swapiResponse, localResults] = await Promise.all(promiseArray);
+    const promiseArray = [
+      swapiResponsePromise,
+      localResultsPromise,
+      deletedEntitiesPromise,
+    ] as const;
 
+    // Use Promise.all to process all promises in parallel
+    const [swapiResponse, localResults, deletedEntitiesResults] =
+      await Promise.all(promiseArray);
+
+    // Filter out deleted entities from the SWAPI API response
+    const deletedEntitiesIds = new Set(
+      deletedEntitiesResults.map(({ entityId }) => entityId)
+    );
+    const filteredSwapiResponse = swapiResponse.results.filter(
+      (entity) => !deletedEntitiesIds.has(entityUrlToId(entity.url))
+    );
+    swapiResponse.results = filteredSwapiResponse;
+
+    // Merge the data from the local database with the data from the SWAPI API
     return Response.json(
       mergeDataSets({
         localResults,
