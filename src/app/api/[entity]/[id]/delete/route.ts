993ca872490693948c6185fb21ab13fc2a15ed51
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2024-07-27 23:59:50 -0500

    update delete logic

--- src/app/api/[entity]/[id]/delete/route.ts
+++ src/app/api/[entity]/[id]/delete/route.ts
@@ -1,11 +1,31 @@
+import { db } from "@/db";
+import { editedEntities } from "@/db/schema";
 import { DynamicIdParams, EntityTypeEnumSchema } from "@/types";
 import { insertDeletedEntity } from "@/utils/entities/insertDeletedEntity";
+import { removeNinePaddedId } from "@/utils/entityIdUtils";
 import { SWAPI } from "@/utils/SWAPI";
+import { eq } from "drizzle-orm";
 
 // I'm using a separate `/delete` endpoint to protect against accidental deletions.
 // This way, it needs to be explicitly set in both the URL and the HTTP method.
 
 export async function DELETE(_request: Request, { params }: DynamicIdParams) {
+  const isEntityLocal = params.id.startsWith("999");
+
+  if (isEntityLocal) {
+    try {
+      // If the entityId is a local entity, delete it from the database.
+      await db
+        .delete(editedEntities)
+        .where(eq(editedEntities.id, removeNinePaddedId(params.id)));
+      return Response.json({
+        message: `Entity with ID ${params.id} has been deleted.`,
+      });
+    } catch (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+  }
+
   // Create a map of entity type to the function that fetches the entity from the SWAPI API.
   const entityMap = {
     people: SWAPI.getPeople,
--- src/utils/entityIdUtils.ts
+++ src/utils/entityIdUtils.ts
@@ -6,6 +6,6 @@ export function updatedEntityIdToNinePaddedId(entityId: number) {
   return "999" + entityId;
 }
 
-export function removeNinePaddedId(entityId: number) {
+export function removeNinePaddedId(entityId: number | string) {
   return Number(entityId.toString().substring(3));
 }
